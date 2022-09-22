0dc8cfed78e2994e8a69f7ac4e4710320c401451
Terry Abu <terryabu67@gmail.com>
2022-09-22 16:16:54 -0700

    updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#define MAIN_H
-#ifndef MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +13,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
